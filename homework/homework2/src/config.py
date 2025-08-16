d7da9906a25976aa179a5ad8bf5b2f201a841232
Chloe4566 <hhhazel.4566@gmail.com>
2025-08-16 15:18:33 -0400

    Add config.py with load_env and get_key functions

--- homework/homework2/src/config.py
+++ homework/homework2/src/config.py
@@ -2,9 +2,11 @@ import os
 from dotenv import load_dotenv
 
 def load_env():
+    """Load environment variables from .env file"""
     load_dotenv()
 
-def get_key(key):
+def get_key(key: str):
+    """Get a value from environment variables"""
     return os.getenv(key)
 
 if __name__ == "__main__":
